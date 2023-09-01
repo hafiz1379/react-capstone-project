ec25e9a897f455a7e76da4a6fb944e70f434ca93
hafiz1379 <hafizrasa1379@gmail.com>
2023-09-01 11:15:30 +0430

    Add test for airPolutionSlice

---
+++ src/test/airPolutionSlice.test.js
@@ -0,0 +1,73 @@
+import { configureStore } from '@reduxjs/toolkit';
+import airPolutionReducer, { getInfo } from '../redux/airPolution/airPolutionSlice';
+
+describe('Air Pollution Slice', () => {
+  let store;
+
+  beforeEach(() => {
+    store = configureStore({
+      reducer: {
+        airPolution: airPolutionReducer,
+      },
+    });
+  });
+
+  it('should have initial state', () => {
+    const initialState = {
+      co: 0,
+      nh3: 0,
+      no: 0,
+      no2: 0,
+      o3: 0,
+      pm2_5: 0,
+      pm10: 0,
+      so2: 0,
+    };
+
+    const state = store.getState().airPolution;
+
+    expect(state).toEqual(initialState);
+  });
+
+  it('should fetch air pollution info correctly', async () => {
+    const coor = [40.0, -4.0];
+    await store.dispatch(getInfo(coor));
+
+    const state = store.getState().airPolution;
+
+    expect(state.co).toBeGreaterThanOrEqual(0);
+    expect(state.nh3).toBeGreaterThanOrEqual(0);
+    expect(state.no).toBeGreaterThanOrEqual(0);
+    expect(state.no2).toBeGreaterThanOrEqual(0);
+    expect(state.o3).toBeGreaterThanOrEqual(0);
+    expect(state.pm2_5).toBeGreaterThanOrEqual(0);
+    expect(state.pm10).toBeGreaterThanOrEqual(0);
+    expect(state.so2).toBeGreaterThanOrEqual(0);
+  });
+
+  it('should update state with fetched data', async () => {
+    const mockData = {
+      co: 1,
+      nh3: 2,
+      no: 3,
+      no2: 4,
+      o3: 5,
+      pm2_5: 6,
+      pm10: 7,
+      so2: 8,
+    };
+
+    const coor = [40.0, -4.0];
+    const response = { list: [{ components: mockData }] };
+
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue(response),
+    });
+
+    await store.dispatch(getInfo(coor));
+
+    const state = store.getState().airPolution;
+
+    expect(state).toEqual(mockData);
+  });
+});
