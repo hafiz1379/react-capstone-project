f6fec367358569267e5b931eb010d0baf90c6674
hafiz1379 <hafizrasa1379@gmail.com>
2023-09-01 11:06:35 +0430

    Update NotFoundPage compo with custom 404 message

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+const NotFoundPage = () => (
+  <div className="flex flex-col items-center justify-center h-screen">
+    <h1 className="text-6xl font-bold mb-4">404</h1>
+    <p className="text-2xl">Sorry, page not found</p>
+  </div>
+);
+export default NotFoundPage;
